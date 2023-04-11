20b459aaaddc79c2871ab285a8f34a036915855e
w0nyv <nagano.tamawo77@gmail.com>
2023-04-11 17:04:01 +0900

    add draw();

--- src/Components/GridModal.tsx
+++ src/Components/GridModal.tsx
@@ -9,7 +9,7 @@ const GridModal: React.FC<ModalBodyProps> = (props) => {
   if(props.isModalShowing) {
     return (
       <div className="absolute inset-0 w-full" onClick={() => props.setIsModalShowing(false)}>
-        <div className="m-auto mt-28 w-10/12 lg:w-4/6 h-2/3 lg:h-5/6 bg-black bg-opacity-90 overflow-auto">
+        <div className="m-auto mt-20 w-10/12 lg:w-4/6 h-2/3 lg:h-5/6 bg-black bg-opacity-70 overflow-auto">
         <img width={48} src="./icons8-close.svg" alt="close modal window" className="ml-auto" />
           {props.children}
         </div>
