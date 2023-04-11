6ce9f1f35fd699221b1609fc6654e4eda57d2d9e
W0NYV <kani.king241@gmail.com>
2023-04-11 22:39:48 +0900

    スケッチページの作成

--- src/Data/SketchesInfo.tsx
+++ src/Data/SketchesInfo.tsx
@@ -11,10 +11,80 @@ type WorksInfo = {
 
 const SketchesInfoArr: WorksInfo[] = [
 
-  {title: "aaaa", 
-  img: "aaaa",
+  {title: "w0nyv VJ Footage AKA", 
+  img: "./images/vjpackaka.webp",
   modalElement: 
   <div>
+    <ModalTitle>w0nyv VJ Footage AKA</ModalTitle>
+    <img className="m-auto w-5/6" src="./images/vjpackaka.webp" alt="vjpackaka" />
+    <div className="mx-8">
+      <p className="mt-3 text-white">赤いVJ素材</p>
+      <p className="mt-3 text-white">Tool: Adobe After Effects, Blender</p>
+      <p className="mt-3 text-white">2022</p>
+    </div>
+    <br /><br />
+  </div>},
+
+  {title: "DosShrimp VJ Footage vol.1", 
+  img: "./images/vjpack.webp",
+  modalElement: 
+  <div>
+    <ModalTitle>DosShrimp VJ Footage vol.1</ModalTitle>
+    <YouTubeEmbed id="PrmrXh74FYA" />
+    <div className="mx-8">
+      <p className="mt-3 text-white">シンプルなVJ素材</p>
+      <p className="mt-3 text-white">Tool: Adobe After Effects</p>
+      <p className="mt-3 text-white">2022</p>
+      <ModalLinks links={[
+          {title: "DosShrimp VJ Footage vol.1 - をにゅ市場 - BOOTH", url: "https://dosshrimp.booth.pm/items/3678997"},
+        ]} />
+    </div>
+    <br /><br />
+  </div>},
+
+  {title: "1K3G4M3X", 
+  img: "./images/ikegame.webp",
+  modalElement: 
+  <div>
+    <ModalTitle>1K3G4M3X</ModalTitle>
+    <img className="m-auto w-5/6" src="https://user-images.githubusercontent.com/15060080/212593531-d5378362-fbdf-404e-93e2-f2bdb5a2b5c8.gif" alt="1K3G4M3X" />
+    <img className="m-auto w-5/6" src="https://user-images.githubusercontent.com/15060080/212593537-1a320219-cd25-472d-9514-ddfeeaeb8fa6.gif" alt="1K3G4M3X" />
+    <div className="mx-8">
+      <p className="mt-3 text-white">人物切り抜き生カメエフェクトツール</p>
+      <p className="mt-3 text-white">Tool: Unity, SelfieSegmentationBarracuda</p>
+      <p className="mt-3 text-white">2023</p>
+    </div>
+    <br /><br />
+  </div>},
+
+  {title: "nanokon2-setup", 
+  img: "./images/nanokon2.webp",
+  modalElement: 
+  <div>
+    <ModalTitle>nanokon2-setup</ModalTitle>
+    <img className="m-auto h-5/6" src="https://user-images.githubusercontent.com/15060080/210543288-45508b9e-0784-4f06-b6e1-e76c017e96fc.gif" alt="nanokon2" />
+    <div className="mx-8">
+      <p className="mt-3 text-white">Unity用のKORG nanoKontrol2セットアップツール</p>
+      <p className="mt-3 text-white">Tool: Unity, UniRx, Input System</p>
+      <p className="mt-3 text-white">2023</p>
+      <ModalLinks links={[
+          {title: "楽にnanoKONTROL2をUnityで動かすためのセットアップツールをつくった", url: "https://w0nyv.hatenablog.com/entry/2023/01/05/015949"},
+        ]} />
+    </div>
+    <br /><br />
+  </div>},
+
+  {title: "nyvvvv-gl", 
+  img: "./images/shaderEditor.webp",
+  modalElement: 
+  <div>
+    <ModalTitle>nyvvvv-gl</ModalTitle>
+    <img className="m-auto h-5/6" src="./images/shaderEditor.gif" alt="ShaderEditor" />
+    <div className="mx-8">
+      <p className="mt-3 text-white">WebGL製のシェーダエディタ</p>
+      <p className="mt-3 text-white">Tool: WebGL</p>
+      <p className="mt-3 text-white">2022</p>
+    </div>
     <br /><br />
   </div>},
 ];
