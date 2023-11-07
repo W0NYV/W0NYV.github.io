04568594b29cd248e525de166bfa99e0a9bdd047
W0NYV <kani.king241@gmail.com>
2023-11-07 10:41:43 +0900

    Twitter -> X

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -3,7 +3,7 @@ function Contact() {
   return (
     <div className="Contact">
       <p className="m-auto text-white text-center mt-10">- Contact -</p>
-      <p className="m-auto text-white text-center">お気軽になんでも<a href="https://twitter.com/W0NYV" className="text-teal-300 hover:text-teal-600">Twitter</a>でDMしてください！</p>
+      <p className="m-auto text-white text-center">お気軽になんでも<a href="https://twitter.com/W0NYV" className="text-teal-300 hover:text-teal-600">X</a>でDMしてください！</p>
     </div>
   );
 }
